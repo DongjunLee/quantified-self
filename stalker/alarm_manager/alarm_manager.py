81c5ce28b4163313e2b22f8ee181b6a150db25ba
DongjunLee <ehdwns2045@gmail.com>
2016-09-16 16:01:22 +0900

    Add: AlarmManager class

---
+++ stalker/alarm_manager/alarm_manager.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class AlarmManager(object):
+
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
