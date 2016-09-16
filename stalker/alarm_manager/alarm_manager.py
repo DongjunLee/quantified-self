25adbca913d8bacd9213d35363139aa8a6f05ec8
DongjunLee <ehdwns2045@gmail.com>
2016-09-16 18:17:14 +0900

    Add: AlarmManager - CRUD

--- stalker/alarm_manager/alarm_manager.py
+++ stalker/alarm_manager/alarm_manager.py
@@ -5,14 +5,18 @@ class AlarmManager(object):
     def __init__(self):
         pass
 
-    def create(self):
-        pass
+    def create(self, params):
+        print("alarm create!!")
+        print(params)
 
-    def read(self):
-        pass
+    def read(self, params):
+        print("alarm read!!")
+        print(params)
 
-    def update(self):
-        pass
+    def update(self, params):
+        print("alarm update!!")
+        print(params)
 
-    def delete(self):
-        pass
+    def delete(self, params):
+        print("alarm delete!!")
+        print(params)
