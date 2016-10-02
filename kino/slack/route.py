e7ddec4f08f74e15858c97e938517787434d0d47
DongjunLee <ehdwns2045@gmail.com>
2016-10-02 21:58:05 +0900

    Add: route State, when state exist -> continue state

--- kino/slack/route.py
+++ kino/slack/route.py
@@ -1,8 +1,9 @@
 import re
 
+from functions.manager import FunctionManager
 from notifier.scheduler import Scheduler
 from notifier.between import Between
-from functions.table import FunctionTable
+from utils.state import State
 
 class MsgRouter(object):
 
@@ -12,6 +13,14 @@ class MsgRouter(object):
     def route(self, msg):
         text = msg["text"]
 
+        state = State()
+        if state.is_do_something():
+            route_class = globals()[state.current["class"]]()
+            func_name = state.current["def"]
+            step_num = state.current["step"]
+            getattr(route_class, func_name)(step=step_num, params=text)
+            return
+
         if text.startswith('알람'):
             re_list = self.__re_alarm_manager()
             route_class = Scheduler()
@@ -20,7 +29,7 @@ class MsgRouter(object):
             route_class = Between()
         elif text.startswith('함수'):
             re_list = self.__re_function_table()
-            route_class = FunctionTable()
+            route_class = FunctionManager()
         else:
             re_list = []
             route_class = object
@@ -32,15 +41,15 @@ class MsgRouter(object):
 
             if result:
                 print("route to: " + route_class.__class__.__name__ + " method: " + func_name)
-                getattr(route_class, func_name)(result.groups())
+                getattr(route_class, func_name)()
                 break
 
     def __re_alarm_manager(self):
         re_alarm_list = [
-            (r'알람 등록 (.*)', 'create'),
+            (r'알람 등록', 'create'),
             (r'알람 보기', 'read'),
-            (r'알람 변경 (.*)', 'update'),
-            (r'알람 삭제 (.*)', 'delete'),
+            (r'알람 변경', 'update'),
+            (r'알람 삭제', 'delete'),
             (r'알람 시작', 'run'),
             (r'알람 중지', 'stop')
         ]
@@ -48,10 +57,10 @@ class MsgRouter(object):
 
     def __re_between(self):
         re_between_list = [
-            (r'시간대 등록 (.*)', 'create'),
+            (r'시간대 등록', 'create'),
             (r'시간대 보기', 'read'),
-            (r'시간대 변경 (.*)', 'update'),
-            (r'시간대 삭제 (.*)', 'delete')
+            (r'시간대 변경', 'update'),
+            (r'시간대 삭제', 'delete')
         ]
         return re_between_list
 
