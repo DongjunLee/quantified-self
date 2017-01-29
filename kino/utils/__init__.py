7e731aa040250ee45899be2594a7ccd1ec156914
DongjunLee <ehdwns2045@gmail.com>
2017-01-29 20:03:38 +0900

    feat(score): add Score class with percent method

--- kino/utils/__init__.py
+++ kino/utils/__init__.py
@@ -4,8 +4,10 @@ from .config import Config
 from .data_handler import DataHandler
 from .logger import Logger
 from .profile import Profile
+from .score import Score
 
 __all__ = ['Config',
            'DataHandler',
            'Logger',
-           'Profile']
+           'Profile',
+           'Score']
---
+++ kino/utils/score.py
@@ -0,0 +1,12 @@
+
+class Score(object):
+
+    def __init__(self):
+        pass
+
+    def percent(self, point, max_point, threshold):
+        if point >= threshold:
+            score = max_point
+        else:
+            score = (max_point * (point/threshold))
+        return score
