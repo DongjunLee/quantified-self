4f64468cdf8b393c8d7c3ce3d1aae6ce9e01d550
DongjunLee <ehdwns2045@gmail.com>
2017-04-23 10:52:03 +0900

    bugfix(requirements): add sklearn and edit dnd import path

--- kino/dialog/dnd.py
+++ kino/dialog/dnd.py
@@ -1,16 +1,16 @@
 
 import arrow
 
-from .nlp.ner import NamedEntitiyRecognizer
+from ..nlp.ner import NamedEntitiyRecognizer
 
-from .skills.summary import Summary
+from ..skills.summary import Summary
 
-from .slack.resource import MsgResource
-from .slack.slackbot import SlackerAdapter
+from ..slack.resource import MsgResource
+from ..slack.slackbot import SlackerAdapter
 
-from .utils.arrow import ArrowUtil
-from .utils.data_handler import DataHandler
-from .utils.state import State
+from ..utils.arrow import ArrowUtil
+from ..utils.data_handler import DataHandler
+from ..utils.state import State
 
 
 class DNDManager(object):
