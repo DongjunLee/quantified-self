7f2abd518376e3c06893cb4c607729cd6069536d
DongjunLee <ehdwns2045@gmail.com>
2017-05-09 23:17:29 +0900

    bugfix(predictor): remove classes using toggl

--- kino/utils/classes.py
+++ kino/utils/classes.py
@@ -6,10 +6,5 @@ class Skill(object):
         (["초지역", "버스"], "bus_stop"),
         (["주간", "작업 리포트"], "toggl_report"),
         (["주간", "커밋"], "github_commit"),
-        (["미세먼지"], "air_quality"),
-        (["toggl Feedly"], "toggl_timer"),
-        (["toggl Inbox Zero"], "toggl_timer"),
-        (["toggl Book"], "toggl_timer"),
-        (["toggl Pocket"], "toggl_timer"),
-        (["toggl Morning Paper"], "toggl_timer")
+        (["미세먼지"], "air_quality")
     ]
