d86c66b9274278258a6b55888bf06fa3bc810aaf
DongjunLee <humanbrain.djlee@gmail.com>
2017-08-20 02:05:59 +0900

    bugfix(holiday): send holiday message logic

--- kino/dialog/dnd.py
+++ kino/dialog/dnd.py
@@ -20,7 +20,7 @@ class DoNotDisturbManager(object):
         # TODO: {'dnd_enabled': True, 'next_dnd_start_ts': 1500711803, 'next_dnd_end_ts': 1500715403}
         # -> dirrenct action with diff_min
         Summary().record_holiday(holiday)
-        if dnd:
+        if dnd or holiday:
             self.slackbot.send_message(text=MsgResource.HOLIDAY)
         else:
             self.slackbot.send_message(text=MsgResource.WEEKDAY)
